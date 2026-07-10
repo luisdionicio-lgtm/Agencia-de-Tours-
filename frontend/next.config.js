b238df64e115731b83a5ddc1d369abc55820af34
Luis Angel Dionicio Bartolo <luisdionicio-lgtm@users.noreply.github.com>
2026-07-09 23:15:34 -0500

    Fix Vercel Next runtime tracing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,6 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  outputFileTracingRoot: path.resolve(__dirname)
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
